193ad6ce0109380b67f25f084dfb5a91d9b9f23f
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-29 20:11:53 +0700

    Tugas 30 JS

---
+++ tugas30.js
@@ -0,0 +1,7 @@
+function regex(value) {
+    var data = value.match(/[ese]/g);
+    
+    console.log(data);
+    
+}
+regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
